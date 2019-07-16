cddbc4b06b34e34c813aa0d10e47058aa8c7332b
Luke Donahue <luke9389@gmail.com>
2019-07-16 16:49:28 -0700

    successful too low test

--- test/compare-numbers.js
+++ test/compare-numbers.js
@@ -1,6 +1,11 @@
-// import compareNumbers from '../src/compare-numbers.js'
+import compareNumbers from '../src/compare-numbers.js';
 
-// const test = QUnit.test;
+const test = QUnit.test;
 
-// test('test number too low', 
-// )
+test('test number too low', function(assert) {
+    const guess = 2;
+    const expected = -1;
+    const result = compareNumbers(guess, 5);
+
+    assert.equal(expected, result);
+});
