6a6a838057fc851bd9ec447c3a9fe2debd20b2a7
Luke Donahue <luke9389@gmail.com>
2019-07-16 10:53:50 -0700

    cleaned up code

--- src/app.js
+++ src/app.js
@@ -1,36 +1,29 @@
 import isYes from './is-yes.js';
 
-//Referencing DOM elements
 const scoreMessage = document.getElementById('quiz-result');
 const quizButton = document.getElementById('quiz-button');
 const numberScore = document.getElementById('numberScore');
 
-
 quizButton.onclick = function() {
-    //Asking user for their name
+
     const userName = prompt('State your name.');
     if(userName === '') {
         alert('You cannot leave a prompt blank! Shame on you!');
         return;
     }
-    //Asking user where they are from
+
     const userHomeland = prompt('From where do you hail ' + userName + ' ?');
     if(userHomeland === '') {
         alert('You cannot leave a prompt blank! Shame on you!');
         return;
     }
 
-    //Verifying that user wants to take quiz
     const confirmQuiz = confirm('Ok ' + userName + ' from ' + userHomeland + ', are you sure you\'d like to take this harrowing quiz?');
     if(!confirmQuiz) {
         return;
     }
 
-    //Making Score Counter Variable
     let score = 0;
-
-
-    //Question One
 
     const birthPlace = prompt('Is it true that I was born in Austin, TX?');
 
@@ -46,11 +39,9 @@ quizButton.onclick = function() {
         score += 1;
     }
 
-    //Question Two
     const food = prompt('Is it true that I dispise sushi?');
     if(!isYes(food)) {
         score += 1;
-
     }
     else if(isYes(food) === 'empty') {
         alert('You cannot leave a prompt blank! Shame on you!');
@@ -61,7 +52,6 @@ quizButton.onclick = function() {
         return;
     }
 
-    //Question Three
     const cat = prompt('Is it true that my girlfriend has a cat named Mochi?');
     if(isYes(cat)) {
         if(isYes(cat) === 'empty') {
@@ -77,20 +67,14 @@ quizButton.onclick = function() {
 
     alert('Thanks for playing! Your results will now be shown at the bottom of the page.');
 
-    //#region
-    //REACH GOAL: COLOR CODE SCORE
-
-    //unhide score box and clear last color
     numberScore.classList.remove('zero');
     numberScore.classList.remove('one');
     numberScore.classList.remove('two');
     numberScore.classList.remove('three');
     numberScore.classList.remove('hidden');
 
-    //Make score fraction & percentage
     numberScore.textContent = score + '/3 = ' + Math.floor((score * 100) / 3) + '%';
 
-    //Change style of quiz-result class
     if(score === 0) {
         numberScore.classList.add('zero');
     }
@@ -101,11 +85,7 @@ quizButton.onclick = function() {
         numberScore.classList.add('two');
     }
     else { numberScore.classList.add('three'); }
-    //#endregion
 
-    //Spit out message for final score
     scoreMessage.textContent = 'Ok ' + userName + ' of ' + userHomeland +
         ', your score is...';
-
-
 };
--- test/tests.js
+++ test/tests.js
@@ -3,71 +3,49 @@ import isYes from '../src/is-yes.js';
 const test = QUnit.test;
 
 test('testing YES', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
     const text = 'YES';
     const expected = true;
-
-    //Act 
-    // Call the function you're testing and set the result to a const
     const result = isYes(text);
 
-    //Assert
     assert.equal(result, expected);
 });
 
 test('testing Yes', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
     const text = 'Yes';
     const expected = true;
-
-    //Act 
-    // Call the function you're testing and set the result to a const
     const result = isYes(text);
 
-    //Assert
     assert.equal(result, expected);
 });
 
 test('testing y', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
     const text = 'y';
     const expected = true;
-
-    //Act 
-    // Call the function you're testing and set the result to a const
     const result = isYes(text);
 
-    //Assert
     assert.equal(result, expected);
 });
 
 test('testing yes', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
     const text = 'yes';
     const expected = true;
-
-    //Act 
-    // Call the function you're testing and set the result to a const
     const result = isYes(text);
 
-    //Assert
     assert.equal(result, expected);
 });
 
 test('testing frisbee is no', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
     const text = 'frisbee';
-    const expected = false;
-
-    //Act 
-    // Call the function you're testing and set the result to a const
+    const expected = 'error';
     const result = isYes(text);
 
-    //Assert
+    assert.equal(result, expected);
+});
+
+test('testing empty string', function(assert) {
+    const text = '';
+    const expected = 'empty';
+    const result = isYes(text);
+
     assert.equal(result, expected);
 });
